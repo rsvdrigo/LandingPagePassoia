c87fd952b202a8d24dbd32d617b5e36e2ab69f4e
rsvdrigo <rsvdrigo@gmail.com>
2025-11-12 10:37:06 -0300

    init(APP): initial project things and create components

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Components/Header";
+
+
+function App(){
+  return(
+  <>
+  <h1>Passoia</h1>
+  <Header/>
+  </>
+  )
+}
+
+export default App;
---
+++ src/Components/Header/Index.jsx
@@ -0,0 +1,20 @@
+
+
+
+function Header(){
+    return(
+        <>
+        <img src="" alt="" />
+        <nav>
+            <ul>
+                <li>LOOKS</li>
+                <li>LANÇAMENTOS</li>
+                <li>NOVIDADE</li>
+            </ul>
+        </nav>
+        </>
+    )
+}
+
+export default Header;
+
