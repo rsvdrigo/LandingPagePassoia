0dbc28acf04cbc1bc0c2ffe7d642ee39fda92ce9
rsvdrigo <rsvdrigo@gmail.com>
2025-11-15 15:39:40 -0300

    feat(Release): add responsive mode to mobile

--- src/Components/Releases/Releases.jsx
+++ src/Components/Releases/Releases.jsx
@@ -5,6 +5,7 @@ import { useState } from "react"
 function Release () {
 
 const [LipsColor, setLipsColor] = useState(RedLips);
+
   return (
     <>
     <section className="release-section">
@@ -23,19 +24,36 @@ const [LipsColor, setLipsColor] = useState(RedLips);
                 <h3>Matte Premium</h3>
                 <p>Cores disponível</p>
                 <div className="release-info-pallet">
-                    <img onMouseOver={()=>setLipsColor(BlueLips)} src={BlueCircle} alt="circulo azul" />
-                    <img onMouseOver={()=>setLipsColor(RedLips)} src={RedCircle} alt="circulo vermelho" />
-                    <img onMouseOver={()=>setLipsColor(BrownLips)} src={BrownCircle} alt="circulo marrom" />
-                    <img onMouseOver={()=>setLipsColor(BaseLips)} src={BaseCircle} alt="circulo cor base" />
+                    <img 
+                        onClick={()=>setLipsColor(BlueLips)} 
+                        onMouseOver={()=>setLipsColor(BlueLips)} 
+                        src={BlueCircle} 
+                        alt="circulo azul" 
+                    />
+                    <img 
+                        onClick={()=>setLipsColor(RedLips)} 
+                        onMouseOver={()=>setLipsColor(RedLips)} 
+                        src={RedCircle} 
+                        alt="circulo vermelho" 
+                    />
+                    <img 
+                        onClick={()=>setLipsColor(BrownLips)} 
+                        onMouseOver={()=>setLipsColor(BrownLips)} 
+                        src={BrownCircle} 
+                        alt="circulo marrom" 
+                    />
+                    <img 
+                        onClick={()=>setLipsColor(BaseLips)} 
+                        onMouseOver={()=>setLipsColor(BaseLips)} 
+                        src={BaseCircle} 
+                        alt="circulo cor base" 
+                    />
                 </div>
                 <h4>Descrição</h4>
                 <h5>O Batom Matte possui uma fórmula inovadora desenvolvida para entregar o máximo de cor na primeira aplicação com um deslize suave e macio. Tem acabamento matte aveludado e manteiga de manga que ajuda a hidratar e a proteger os lábios contra ressecamento. Ajuda na hidratação dos lábios, textura fina e macia que não pesa nos lábios.</h5>
-                
             </div>
         </div>
     </section>
-      
-    
     </>
   )
 }
