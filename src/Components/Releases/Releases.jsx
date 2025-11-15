7b7e85f1c0a4a8d67dac046c70f1980b2fd34dfb
rsvdrigo <rsvdrigo@gmail.com>
2025-11-15 10:48:39 -0300

    feat(Release): add style web to release

--- src/Components/Releases/Releases.jsx
+++ src/Components/Releases/Releases.jsx
@@ -19,7 +19,7 @@ const [LipsColor, setLipsColor] = useState(RedLips);
                 <img src={LipsColor} alt="Imagem de uma mulher com batom" />
             </div>
             <div className="release-info">
-                <img src={Stars} alt="estrelas de avaliacao"/>
+                <img className="release-stars-info" src={Stars} alt="estrelas de avaliacao"/>
                 <h3>Matte Premium</h3>
                 <p>Cores disponível</p>
                 <div className="release-info-pallet">
@@ -28,7 +28,8 @@ const [LipsColor, setLipsColor] = useState(RedLips);
                     <img onMouseOver={()=>setLipsColor(BrownLips)} src={BrownCircle} alt="circulo marrom" />
                     <img onMouseOver={()=>setLipsColor(BaseLips)} src={BaseCircle} alt="circulo cor base" />
                 </div>
-                <h4>O Batom Matte possui uma fórmula inovadora desenvolvida para entregar o máximo de cor na primeira aplicação com um deslize suave e macio. Tem acabamento matte aveludado e manteiga de manga que ajuda a hidratar e a proteger os lábios contra ressecamento. Ajuda na hidratação dos lábios, textura fina e macia que não pesa nos lábios.</h4>
+                <h4>Descrição</h4>
+                <h5>O Batom Matte possui uma fórmula inovadora desenvolvida para entregar o máximo de cor na primeira aplicação com um deslize suave e macio. Tem acabamento matte aveludado e manteiga de manga que ajuda a hidratar e a proteger os lábios contra ressecamento. Ajuda na hidratação dos lábios, textura fina e macia que não pesa nos lábios.</h5>
                 
             </div>
         </div>
