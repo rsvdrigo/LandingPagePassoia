a743d4d5a90c672ad9d769617f23ea36068ce237
rsvdrigo <rsvdrigo@gmail.com>
2025-11-15 15:29:09 -0300

    feat(Looks): add mobile mode to max-width 768px

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,22 +1,54 @@
 import "./Header.scss";
-import Logo from "../../assets/imgs/Logo Passoia.png"
+import Logo from "../../assets/imgs/Logo Passoia.png";
+import { useState } from "react";
 
 function Header(){
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    const closeMenu = () => {
+        setIsMenuOpen(false);
+    };
+
     return(
         <>
-        <div className="container">
-        <img src={Logo} alt="imagem do logo passoia" />
-        <nav className="navbar">
-            <ul>
-                <li><a href="#">LOOKS</a></li>
-                <li><a href="">LANÇAMENTOS</a></li>
-                <li><a href="#">NOVIDADE</a></li>
-            </ul>
-        </nav>
-        </div>
+            <div className="container">
+                <img src={Logo} alt="imagem do logo passoia" />
+                
+                {/* Menu Desktop */}
+                <nav className="navbar">
+                    <ul>
+                        <li><a href="#">LOOKS</a></li>
+                        <li><a href="#">LANÇAMENTOS</a></li>
+                        <li><a href="#">NOVIDADE</a></li>
+                    </ul>
+                </nav>
+
+                {/* Menu Hamburger */}
+                <div 
+                    className={`hamburger ${isMenuOpen ? 'active' : ''}`} 
+                    onClick={toggleMenu}
+                >
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+
+            {/* Menu Mobile */}
+            <div className={`mobile-menu ${isMenuOpen ? 'active' : ''}`}>
+                <button className="close-menu" onClick={closeMenu}>×</button>
+                <ul>
+                    <li><a href="#" onClick={closeMenu}>LOOKS</a></li>
+                    <li><a href="#" onClick={closeMenu}>LANÇAMENTOS</a></li>
+                    <li><a href="#" onClick={closeMenu}>NOVIDADE</a></li>
+                </ul>
+            </div>
         </>
     )
 }
 
 export default Header;
-
